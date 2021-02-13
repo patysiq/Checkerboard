5103849b1bfa2383674a3a3ada93ed12d7fc0b47
Patricia <patysiq@yahoo.com.br>
2021-02-13 10:50:14 -0300

    Initial Commit

---
+++ Checkerboard/CheckerboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckerboardApp.swift
+//  Checkerboard
+//
+//  Created by PATRICIA S SIQUEIRA on 13/02/21.
+//
+
+import SwiftUI
+
+@main
+struct CheckerboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
